d6b5ba9783b3918b51c3be88e490447d3d5dc043
Ayomi Marjan <marjanamonir02@gmail.com>
2025-09-03 20:58:05 +0600

    creating dropdown using SelectListItem

---
+++ proj_m/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using proj_m.Models.ViewModels;
+using SyncSyntax.Data;
+
+namespace proj_m.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PostController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            var postViewModel = new PostViewModel();
+            postViewModel.Categories = _context.Categories.Select(c =>
+            new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Name
+            }
+            ).ToList();
+            return View();
+        }
+    }
+}
